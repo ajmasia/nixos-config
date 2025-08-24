0388d0ed137b484b42daf3393bcbe70fd9bd893c
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-08-24 16:12:09 +0200

    fix: typo in default.nix includes

--- nixos/includes/default.nix
+++ nixos/includes/default.nix
@@ -1,5 +1,7 @@
 # Aggregates common, reusable parts. Hosts import this directory (../includes).
-{ ... }: {
+{ ... }:
+
+{
   imports = [
     ./nixpkgs-configuration.nix
     ./nix-configuration.nix
