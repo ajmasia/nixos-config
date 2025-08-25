6abb98baebeca3549fdae7d00e0886da4657fe89
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-08-25 14:04:47 +0200

    conf: improve install script

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ REPO_URL="https://github.com/ajmasia/nixos-config.git"
 CLONE_DIR="$HOME/.nixos-config"
 HOST_NAME="lab" # Change this to your actual host flake name if needed
 
-echo "Starting NixOS configuration installation for host '$HOST_NAME'..."
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Starting NixOS configuration installation for host '$HOST_NAME'...${NC}"
 
 # Ensure nix-shell is available
 if ! command -v nix-shell >/dev/null 2>&1; then
@@ -17,18 +22,18 @@ fi
 nix-shell -p git --run "
   # Clone the repo if not already present
   if [ ! -d \"$CLONE_DIR\" ]; then
-    echo 'Cloning configuration repository...'
+    echo -e '${YELLOW}Cloning configuration repository...${NC}'
     git clone \"$REPO_URL\" \"$CLONE_DIR\"
   else
-    echo 'Repository already exists at $CLONE_DIR, pulling latest changes...'
-    echo 'Repo already cloned at $CLONE_DIR'
+    echo -e '${YELLOW}Repository already exists at $CLONE_DIR, pulling latest changes...${NC}'
+    echo -e '${YELLOW}Repo already cloned at $CLONE_DIR${NC}'
   fi
 "
 
-cd "$CLONE_DIR"
-
 # Apply the configuration with flakes enabled
-echo "Applying NixOS configuration using flakes..."
+echo -e "${GREEN}Applying NixOS configuration using flakes...${NC}"
+
+cd "$CLONE_DIR"
 sudo nixos-rebuild switch --impure --flake "$CLONE_DIR#$HOST_NAME" --option experimental-features "nix-command flakes"
 
-echo "NixOS configuration applied successfully!"
+echo -e "${GREEN}NixOS configuration applied successfully!${NC}"
