9932403d8c1748577828f66267c959621cde359b
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-08-25 08:00:36 +0200

    conf: improve greetd config

--- nixos/host/vm/includes/loggin.nix
+++ nixos/host/vm/includes/loggin.nix
@@ -2,11 +2,13 @@
 
 {
   # Initial login experience
+  # https://github.com/apognu/tuigreet
   services.greetd = {
     enable = true;
+
     settings = rec {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd bash";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --user-menu --cmd bash";
       };
     };
   };
