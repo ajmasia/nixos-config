6383f5e29592bdf4f264b17f425220e6db7fcd87
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-08-25 08:03:55 +0200

    conf: add tuigreet theme

--- nixos/host/vm/includes/loggin.nix
+++ nixos/host/vm/includes/loggin.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  catppuccinMocha = ''
+    border=lightmagenta;
+    title=lightmagenta;
+    text=white;
+    greet=white;
+    prompt=lightcyan;
+    input=lightgreen;
+    time=lightblue;
+    action=lightblue;
+    button=yellow;
+    container=black
+  '';
 
+  theme = catppuccinMocha;
+in
 {
   # Initial login experience
   # https://github.com/apognu/tuigreet
@@ -8,7 +23,12 @@
 
     settings = rec {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --user-menu --cmd bash";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet \
+          --time \
+          --asterisks \
+          --user-menu \
+          --theme '${lib.replaceStrings ["\n" " "] ["" ""] theme}' \
+          --cmd bash";
       };
     };
   };
