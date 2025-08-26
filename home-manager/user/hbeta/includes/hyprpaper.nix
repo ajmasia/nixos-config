09a1a92bdb0390bd242a9f131952f2290fcfaa26
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-08-26 16:36:56 +0200

    conf: disable hyprpaper

--- home-manager/user/hbeta/includes/hyprpaper.nix
+++ home-manager/user/hbeta/includes/hyprpaper.nix
@@ -8,16 +8,16 @@
     };
   };
 
-  services.hyprpaper = {
-    enable = true;
-
-    settings = {
-      preload = [
-        "$HOME/.config/wallpapers/wallpaper_001.jpg"
-      ];
-      wallpaper = [
-        "$HOME/.config/wallpapers/wallpaper_001.jpg"
-      ];
-    };
-  };
+  # services.hyprpaper = {
+  #   enable = true;
+  #
+  #   settings = {
+  #     preload = [
+  #       "$HOME/.config/wallpapers/wallpaper_001.jpg"
+  #     ];
+  #     wallpaper = [
+  #       "$HOME/.config/wallpapers/wallpaper_001.jpg"
+  #     ];
+  #   };
+  # };
 }
